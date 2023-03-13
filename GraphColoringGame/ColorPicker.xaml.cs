999543ca83d90a83c35dfab7ef7fb8224ec0f803
Charlotte <csbe@itu.dk>
2023-03-13 16:47:19 +0100

    added gitignore, ColorPicker

---
+++ GraphColoringGame/ColorPicker.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace GraphColoringGame
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ColorPicker : UserControl
+    {
+        private List<Graphs.Color> _colors;
+        private int _widthHeight;
+        public Graphs.Color currentSelectColor = Graphs.Color.None;
+        public ColorPicker(List<Graphs.Color> colors)
+        {
+            InitializeComponent();
+            _colors = colors;
+            _widthHeight = 40;
+            for (int i = 0; i < _colors.Count(); i++) addColorButton(_colors[i], i);
+            addColumns(2, new GridLength(pixels: 40));
+            addRows(1, new GridLength(pixels: 50));
+
+
+        }
+        private void addColumns(int cols, GridLength size)
+        {
+            for (int i = 0; i < cols; i++) ColorPickerGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = size });
+        }
+        private void addRows(int rows, GridLength size)
+        {
+            for (int i = 0; i < rows; i++) ColorPickerGrid.RowDefinitions.Add(new RowDefinition() { Height = size });
+        }
+
+        // adds a Button to the ColorPicker with color corresponding to the Color enum from Graphs
+        private void addColorButton(Graphs.Color color, int index)
+        {
+            var Cbutton = new CButton(color) { Uid = $"{color}_btn", Height = _widthHeight, Width = 50, };
+
+            colorButton(Cbutton, color);
+            Cbutton.Click += RedColor_Click;
+            Grid.SetColumn(Cbutton, index);
+            ColorPickerGrid.Children.Add(Cbutton);
+
+        }
+
+        private void colorButton(CButton cb, Graphs.Color color)
+        {
+            switch (color)
+            {
+                case Graphs.Color.Red:
+                    cb.Background = new SolidColorBrush(Colors.Red);
+                    break;
+                case Graphs.Color.Blue:
+                    cb.Background = new SolidColorBrush(Colors.Blue);
+                    break;
+                case Graphs.Color.Green:
+                    cb.Background = new SolidColorBrush(Colors.Green);
+                    break;
+                case Graphs.Color.Yellow:
+                    cb.Background = new SolidColorBrush(Colors.Yellow);
+                    break;
+            }
+
+        }
+        private void RedColor_Click(object sender, RoutedEventArgs e)
+        {
+            // Change color emun
+            var cB = sender as CButton;
+            // Get the color of the button and change the global one.
+            // current selectedColor = cB.ColorV;
+            currentSelectColor = cB.ColorV;
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+            result = MessageBox.Show($"Color of Button: {cB?.ColorV}", "testing", button, icon, MessageBoxResult.Yes);
+
+
+        }
+    }
+    class CButton : Button
+    {
+        public Graphs.Color ColorV;
+
+        public CButton(Graphs.Color color)
+        {
+            ColorV = color;
+        }
+    }
+}
