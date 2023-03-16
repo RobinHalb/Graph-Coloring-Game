c2cf4041383e33d484a9e589bf042e3bf5b9905e
Charlotte Schack Berg <csbe@itu.dk>
2023-03-16 16:29:05 +0100

    Draft for Rules Window

--- GraphColoringGame/RuleWindow.xaml.cs
+++ GraphColoringGame/RuleWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace GraphColoringGame
         {
             InitializeComponent();
         }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
