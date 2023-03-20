68771dfa5bd077e4723b2e31b05ee453249251cf
Charlotte Schack Berg <csbe@itu.dk>
2023-03-20 15:20:29 +0100

    added image folder + indent some text

--- GraphColoringGame/RuleWindow.xaml.cs
+++ GraphColoringGame/RuleWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GraphColoringGame
 {
@@ -19,14 +21,40 @@ namespace GraphColoringGame
     /// </summary>
     public partial class RuleWindow : Window
     {
+
+        private Thickness imageMargin = new Thickness(0,-20,0,-20);
+        private int imageWidth = 250;
+        private int imageHeight = 200;
         public RuleWindow()
         {
+            
+            this.Owner = App.Current.MainWindow;
             InitializeComponent();
+            addUncolorableImage();
         }
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+        private void addUncolorableImage() 
+        {
+            var uncolorableBitImage = addBitImage(200, @"Images\Rules\example_uncolor_vertices.png");
+
+            ImageExample1.Margin = imageMargin;
+            ImageExample1.Width = imageWidth;
+            ImageExample1.Height = imageHeight;
+            ImageExample1.Source = uncolorableBitImage;
+        }
+
+        private BitmapImage addBitImage(int height, string path) 
+        {
+            var bitMapImage = new BitmapImage();
+            bitMapImage.BeginInit();
+            bitMapImage.UriSource = new Uri(path, UriKind.Relative);
+            bitMapImage.DecodePixelHeight = 200;
+            bitMapImage.EndInit();
+            return bitMapImage;
         }
     }
 }
