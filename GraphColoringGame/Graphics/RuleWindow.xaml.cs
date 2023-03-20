3532483ad3d0dda71262a422c39adab0d6a00c73
Charlotte Schack Berg <csbe@itu.dk>
2023-03-20 21:12:47 +0100

    added new image (might have to re-edit) added extra text - need spell checking

---
+++ GraphColoringGame/Graphics/RuleWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraphColoringGame
+{
+    /// <summary>
+    /// Interaction logic for RuleWindow.xaml
+    /// </summary>
+    public partial class RuleWindow : Window
+    {
+
+        private Thickness imageMargin = new Thickness(0,-15,0,-20);
+        private int imageWidth = 250;
+        private int imageHeight = 200;
+        public RuleWindow()
+        {
+            
+            this.Owner = App.Current.MainWindow;
+            InitializeComponent();
+            addUncoloredImage();
+            addUncolorableVImage();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void addUncoloredImage() 
+        {
+            var uncoloredBitImage = addBitImage(200, @"..\Images\Rules\example_uncolor_vertices.png");
+            addImageAttributes(ImageExample1, uncoloredBitImage);
+        }
+
+        private void addUncolorableVImage() 
+        {
+            var uncolorableVBitImage = addBitImage(200, @"..\Images\Rules\example_uncolorable_vertex.png");
+            addImageAttributes(RulesLostGame, uncolorableVBitImage);
+        }
+
+        private BitmapImage addBitImage(int height, string path) 
+        {
+            var bitMapImage = new BitmapImage();
+            bitMapImage.BeginInit();
+            bitMapImage.UriSource = new Uri(path, UriKind.Relative);
+            bitMapImage.DecodePixelHeight = 200;
+            bitMapImage.EndInit();
+            return bitMapImage;
+        }
+
+        private void addImageAttributes(Image img, BitmapImage bitImage) 
+        {
+            img.Margin = imageMargin;
+            img.Width = imageWidth;
+            img.Height = imageHeight;
+            img.Source = bitImage;
+
+        }
+    }
+}
