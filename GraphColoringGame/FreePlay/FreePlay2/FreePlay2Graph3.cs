9034a57fa3f0402092fd5ff68d74e7cb6b66e40a
RobinHalb <robin.hal6erg@gmail.com>
2023-04-29 22:36:33 +0200

    Added FreePlay2Graph3

---
+++ GraphColoringGame/FreePlay/FreePlay2/FreePlay2Graph3.cs
@@ -0,0 +1,43 @@
+﻿using GraphColoringGame.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphColoringGame.FreePlay
+{
+    /*
+     *      0
+     *      |
+     *  0 - 0 - 0 
+     *      |
+     *      0
+     */
+    public class FreePlay2Graph3 : IFreePlay
+    {
+        public Graph createGraph()
+        {
+            var builder = new GraphBuilder(new List<Color>() { Color.Red, Color.Blue });
+            Coord[] coords = {
+                new Coord(0,1), // coords[0]
+                new Coord(1, 1), // 1
+                new Coord(2, 1), //2
+                new Coord(1, 0),//3
+                new Coord(1, 2),//4
+            };
+
+            builder.addVertexMany(coords);
+
+            (Coord, Coord)[] connections = {
+                (coords[0], coords[1]),
+                (coords[1], coords[2]),
+                (coords[1], coords[3]),
+                (coords[1], coords[4]),
+            };
+            builder.connectVerticesMany(connections);
+
+            return builder.build();
+        }
+    }
+}
