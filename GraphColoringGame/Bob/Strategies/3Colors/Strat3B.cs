523990da936b1bb247f9082dd53b9bc27394bb79
RobinHalb <robin.hal6erg@gmail.com>
2023-05-08 18:52:26 +0200

    Improved Strat3B

--- GraphColoringGame/Bob/Strategies/3Colors/Strat3B.cs
+++ GraphColoringGame/Bob/Strategies/3Colors/Strat3B.cs
@@ -34,8 +34,8 @@ namespace GraphColoringGame.Bob.Strategies.ThreeColors
          * The given vertex is placed as v2 in the pattern.
          * For each possible vertex in each place in the pattern, it is checked whether the requirements are met.
          * - v1 must be colored.
-         * - v2 must have at least two uncolored neighbours, with no colored neighbours themselves.
-         * - v3 must be a dangerous vertex, and have at least two uncolored neighbours, one of which may have one colored neighbour.
+         * - v2 must have at least three uncolored neighbours with some color different from v1 available, and at least two of them must have 3 available colors.
+         * - v3 must be a dangerous vertex, and have at least four uncolored neighbours, one of which must be different from v2 and v4 and have 3 available colors.
          * - v4 must be uncolored, and have the color of v1 available.
          */
         public (Coord, Color)? tryMove(Vertex v2)
@@ -43,9 +43,32 @@ namespace GraphColoringGame.Bob.Strategies.ThreeColors
             var v1 = v2.neighbours.Values.FirstOrDefault(n => n.isColored);
             if (!v2.isDangerous || v1 == null) return null;
 
+            var v2n = v2.neighbours.Values.Where(n => !n.isColored && n.availableColors.Count == 3);
+
+            if (v2n.Count() == 3 ||
+                (v2n.Count() == 2 &&
+                 v2.neighbours.Values.Count(n => !n.isColored && !(n.availableColors.Count() == 1 && n.availableColors.Contains(v1.color))) >= 3))
+            {
+                foreach (var v3 in v2n)
+                {
+                    if (v3.isDangerous && v3.neighbours.Values.Count(n => !n.isColored) >= 4)
+                    {
+                        foreach (var v4 in v3.neighbours.Values)
+                        {
+                            if (!v4.isColored && v4 != v2 && v4.availableColors.Contains(v1.color) &&
+                                v3.neighbours.Values.Any(n => n != v2 && n != v4 && n.availableColors.Count == 3))
+                            {
+                                return (v4.coord, v1.color);
+                            }
+                        }
+                    }
+                }
+            }
+            /*
             foreach (var v3 in v2.neighbours.Values)
             {
                 if (v3.isDangerous &&
+                    !v3.neighbours.Values.Any(n => n.isColored) &&
                     v2.neighbours.Values.Count(n => n != v3 && !n.isColored && n.availableColors.Count == 3) >= 2)
                 {
                     foreach (var v4 in v3.neighbours.Values)
@@ -62,6 +85,7 @@ namespace GraphColoringGame.Bob.Strategies.ThreeColors
                     }
                 }
             }
+            */
             return null;
         }
     }
